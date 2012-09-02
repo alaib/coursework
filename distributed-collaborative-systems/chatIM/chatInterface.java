6b3fa1a64087bbe77f14924992fa7d00b6758c94
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2012-09-02 17:01:03 -0400

    N-User IM

--- distributed-collaborative-systems/chatIM/chatInterface.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface chatInterface extends Remote {
-    public String[] handleEvent(String cName, String cStatus, String msg, int STATUS_CODE) throws RemoteException;    
-}
