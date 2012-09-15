4f0538ff34530854770de726014e20bb00da375d
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2012-09-15 14:47:06 -0400

    Modified files

--- machine-learning/hw1/fdsigmoid.m
+++ machine-learning/hw1/fdsigmoid.m
@@ -1,5 +1,7 @@
 function  d  =  fdsigmoid(z)
+    h = 1e-5;
+    %h=10^-5;
     f0  =  1/(1  +  exp(-z));
-    f1  =  1/(1  +  exp(-(z  +  1e-5)));
-    d  =  (f1  -  f0)/1e-5;
+    f1  =  1/(1  +  exp(-(z  +  h)));
+    d  =  (f1  -  f0)/h;
 end
--- machine-learning/hw1/prob6.m
+++ machine-learning/hw1/prob6.m
@@ -1,12 +1,14 @@
 zs  =  randn(100,1);
+%zs = [0];
 err = zeros(length(zs),1);
 for  i=1:length(zs)
+    display(dsigmoid(zs(i)));
+    display(fdsigmoid(zs(i)));
     err(i)  =  dsigmoid(zs(i))  -  fdsigmoid(zs(i));
 end
 hist(err,30)
 maxError = max(err);
 minError = min(err);
-%fprintf('The error ranges between %f to %f\n', minError, maxError);
 display(strcat('Min Error = ', num2str(minError)));
 display(strcat('Max Error = ', num2str(maxError)));
 hwplotprep
