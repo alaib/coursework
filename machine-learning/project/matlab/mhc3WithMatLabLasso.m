bceb4ca95d683b25858436959a444e39a827c8a1
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2012-12-04 17:43:30 -0500

    Lasso and SVM finally match w.r.t accuracy

--- machine-learning/project/matlab/mhc3WithMatLabLasso.m
+++ machine-learning/project/matlab/mhc3WithMatLabLasso.m
@@ -1,16 +1,25 @@
 clc;
 clear;
-load('train_encoded.mat');
-load('test_encoded.mat');
-alpha = 1.00;
+% Load files
+type = 'HLA-DRB10401';
+trainMatFile = strcat('/home/ravikirn/mlcode/data/MHCBN-15mers/matfiles/', type, '_train_encoded.mat');
+testMatFile = strcat('/home/ravikirn/mlcode/data/MHCBN-15mers/matfiles/', type, '_test_encoded.mat');
+load(trainMatFile);
+load(testMatFile);
+
+
+alpha = 1.0;
 maxIter = 1000;
 
-[B, FitInfo] = lasso(trainX, trainY, 'Alpha', alpha);
+[B, FitInfo] = lasso(trainX, trainY, 'Alpha', alpha, 'CV', 10);
+index = FitInfo.Index1SE;
+predBeta = B(:, index);
+predBeta0 = FitInfo.Intercept(index);
 
-predBeta = B(:,1);
-predictY = testX * predBeta;
+predictY = predBeta0 + testX * predBeta;
 match = 0;
 pY = zeros(size(predictY, 1), 1);
+
 for i = 1 : size(predictY, 1)
     if(predictY(i) > 0.5)
         pY(i) = 1;
@@ -23,7 +32,3 @@ for i = 1 : size(predictY, 1)
 end
 
 fprintf('Prediction Success Rate = %.2f\n', match/size(testY,1)*100);
-
-
-
-
