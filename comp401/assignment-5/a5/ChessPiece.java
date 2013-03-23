b18c4c8dce140c7ef49b24697704a1eb14f200a6
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2013-03-23 15:00:30 -0400

    Finished Pawn

---
+++ comp401/assignment-5/a5/ChessPiece.java
@@ -0,0 +1,201 @@
+package a5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ChessPiece {
+
+	private ChessPlayer owner;
+	private ChessGame game;
+	private ChessPosition position;
+	protected char mark;
+	
+	protected ChessPiece(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		this.owner = owner;
+		this.game = game;
+		this.position = null;
+		
+		game.getBoard().placePieceAt(this, init_position);
+	}
+	
+	public ChessPlayer getOwner() {
+		return owner;
+	}
+	
+	public ChessGame getGame() {
+		return game;
+	}
+	
+		
+	public ChessPosition getPosition() {
+		return position;
+	}
+	
+	public void setPosition(ChessPosition new_position) {
+		position = new_position;
+	}
+	
+	public void moveTo(ChessPosition destination) throws IllegalMove{
+		//Valid move for the piece, check if there is collision or not
+		ChessBoard board = this.getGame().getBoard();
+		ChessPiece prevPiece = board.getPieceAt(destination);
+		
+		if(prevPiece != null){
+			//position is occupied by a piece
+			if(prevPiece.getOwner() == this.getOwner()){
+				//the position is occupied by a piece of the same player
+				throw new IllegalMove(this, position, destination);
+			}else{
+				//remove opponent piece from board
+				board.removePieceFrom(destination);
+				
+				//remove current piece from old pos and put it in new position
+				ChessPiece p = board.getPieceAt(this.position);
+				board.removePieceFrom(this.position);
+				board.placePieceAt(p, destination);
+			}
+		}else{
+			//position is free
+			//remove current piece from old pos and put it in new position
+			ChessPiece p = board.getPieceAt(this.position);
+			board.removePieceFrom(this.position);
+			board.placePieceAt(p, destination);
+		}
+	}
+	
+	public char getMark() {
+		return mark;
+	}
+}
+
+class Rook extends ChessPiece {
+	public Rook(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'r';
+		} else {
+			mark = 'R';
+		}
+	}	
+}
+
+class Bishop extends ChessPiece {
+	public Bishop(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'b';
+		} else {
+			mark = 'B';
+		}
+	}
+}
+
+class Knight extends ChessPiece {
+	public Knight(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'n';
+		} else {
+			mark = 'N';
+		}
+	}
+}
+
+class Queen extends ChessPiece {
+	public Queen(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'q';
+		} else {
+			mark = 'Q';
+		}
+	}	
+}
+
+class King extends ChessPiece {
+	public King(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'k';
+		} else {
+			mark = 'K';
+		}
+	}
+}
+
+class Pawn extends ChessPiece {
+	boolean firstMove;
+	
+	public Pawn(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'p';
+		} else {
+			mark = 'P';
+		}
+		firstMove = true;
+	}
+	
+	public void moveTo(ChessPosition destination) throws IllegalMove{
+		ChessPosition pos = this.getPosition();
+		ChessGame game = this.getGame();
+		ChessBoard board = game.getBoard();
+		ChessPlayer owner = this.getOwner();
+		
+		int dx = 1;
+		int dy = -1;
+		if(owner == game.getPlayer1()){
+			dy = 1;
+		}
+		
+		//Get a list of valid positions and check if destination is one of them, else throw IllegalMove
+		//2 spaces ahead and free slot
+		if(firstMove){
+			ChessPosition p1 = new ChessPosition(pos.getX(), pos.getY() + 2 * dy);
+			if(destination.equals(p1) && board.getPieceAt(p1) == null){
+				super.moveTo(destination);
+				firstMove = false;
+				return;
+			}
+		}
+		
+		List<ChessPosition> validMoves = new ArrayList<ChessPosition>();
+		//1 space ahead and free slot
+		try{
+			ChessPosition p2 = new ChessPosition(pos.getX(), pos.getY() + dy);
+			if(board.getPieceAt(p2) == null){
+				validMoves.add(p2);
+			}
+		}catch(IllegalArgumentException e){
+			//Don't do anything, out of board coordinates
+		}
+		
+		//right diagonal capture
+		try{
+			ChessPosition p3 = new ChessPosition(pos.getX() + dx, pos.getY() + dy);
+			ChessPiece p3Piece = board.getPieceAt(p3);
+			if(p3Piece != null && p3Piece.getOwner() != owner){
+				validMoves.add(p3);
+			}
+		}catch(IllegalArgumentException e){
+			//Don't do anything, out of board coordinates
+		}
+		
+		//left diagonal capture
+		try{
+			ChessPosition p4 = new ChessPosition(pos.getX() - dx, pos.getY() + dy);
+			ChessPiece p4Piece = board.getPieceAt(p4);
+			if(p4Piece != null && p4Piece.getOwner() != owner){
+				validMoves.add(p4);
+			}
+		}catch(IllegalArgumentException e){
+			//Don't do anything, out of board coordinates
+		}
+		
+		if(validMoves.contains(destination)){
+			super.moveTo(destination);
+		}else{
+			throw new IllegalMove(this, pos, destination);
+		}
+	}
+}
---
+++ comp401/assignment-5/a5/ChessPlayer.java
@@ -0,0 +1,14 @@
+package a5;
+
+public class ChessPlayer {
+
+	private String name;
+	
+	public ChessPlayer(String name) {
+		this.name = name;
+	}	
+	
+	public String getName() {
+		return name;
+	}
+}
