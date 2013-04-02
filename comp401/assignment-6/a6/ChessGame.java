c2fa4d91728fe19d8831f54316079758d7217d23
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2013-04-02 14:38:46 -0400

    Added assignment 6

---
+++ comp401/assignment-6/a6/ChessGame.java
@@ -0,0 +1,131 @@
+package a6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+public class ChessGame extends java.util.Observable implements
+		java.util.Observer {
+	private ChessBoard board;
+	private ChessPlayer player1;
+	private ChessPlayer player2;
+	private List<ChessMove> log;
+
+	public ChessGame(ChessPlayer player1, ChessPlayer player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+		log = new ArrayList<ChessMove>();
+		board = new ChessBoard();
+
+		new Rook(player1, this, new ChessPosition(0, 0));
+		new Knight(player1, this, new ChessPosition(1, 0));
+		new Bishop(player1, this, new ChessPosition(2, 0));
+		new Queen(player1, this, new ChessPosition(3, 0));
+		new King(player1, this, new ChessPosition(4, 0));
+		new Bishop(player1, this, new ChessPosition(5, 0));
+		new Knight(player1, this, new ChessPosition(6, 0));
+		new Rook(player1, this, new ChessPosition(7, 0));
+
+		for (int i = 0; i < 8; i++) {
+			new Pawn(player1, this, new ChessPosition(i, 1));
+		}
+
+		new Rook(player2, this, new ChessPosition(0, 7));
+		new Knight(player2, this, new ChessPosition(1, 7));
+		new Bishop(player2, this, new ChessPosition(2, 7));
+		new Queen(player2, this, new ChessPosition(3, 7));
+		new King(player2, this, new ChessPosition(4, 7));
+		new Bishop(player2, this, new ChessPosition(5, 7));
+		new Knight(player2, this, new ChessPosition(6, 7));
+		new Rook(player2, this, new ChessPosition(7, 7));
+
+		for (int i = 0; i < 8; i++) {
+			new Pawn(player2, this, new ChessPosition(i, 6));
+		}
+	}
+
+	public ChessPlayer getPlayer1() {
+		return player1;
+	}
+
+	public ChessPlayer getPlayer2() {
+		return player2;
+	}
+
+	public ChessBoard getBoard() {
+		return board;
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// Type cast arg to ChessMove and add to log
+		ChessMove m = (ChessMove) arg;
+		log.add(m);
+
+		// Call setChanged
+		setChanged();
+		// Notify observers
+		notifyObservers(arg);
+	}
+
+	public int getLogSize() {
+		return log.size();
+	}
+
+	public ChessMove[] getMoves(int n) {
+		int lSize = this.getLogSize();
+		ChessMove[] moves;
+
+		if (lSize == 0) {
+			moves = new ChessMove[0];
+			return moves;
+		}
+
+		if (n == 0 || Math.abs(n) > lSize) {
+			moves = new ChessMove[lSize];
+			moves = log.toArray(moves);
+		} else {
+			int absN = Math.abs(n);
+			moves = new ChessMove[absN];
+			int dir = 1;
+			int index = 0;
+			int k = 0;
+			if (n < 0) {
+				index = lSize - 1;
+				dir = -1;
+			}
+			while (absN > 0) {
+				moves[k++] = log.get(index);
+				index = index + dir;
+				absN--;
+			}
+		}
+		return moves;
+	}
+
+	public void undo() {
+		int lSize = getLogSize();
+		if (lSize == 0) {
+			return;
+		}
+		ChessMove m = log.remove(lSize - 1);
+		ChessPiece p = m.getPiece();
+		ChessPiece captured = m.getCaptured();
+		ChessPosition from = m.getFrom();
+		ChessPosition to = m.getTo();
+
+		// Move the piece -> "from"
+		board.removePieceFrom(to);
+		board.placePieceAt(p, from);
+
+		// Restore captured piece at "to"
+		if (captured != null) {
+			board.placePieceAt(captured, to);
+		}
+		// Call setChanged
+		setChanged();
+		// Notify observers
+		notifyObservers(null);
+
+	}
+}
---
+++ comp401/assignment-6/a6/ChessPiece.java
@@ -0,0 +1,325 @@
+package a6;
+
+import java.util.Observer;
+
+public abstract class ChessPiece extends java.util.Observable{
+
+	private ChessPlayer owner;
+	private ChessGame game;
+	private ChessPosition position;
+	protected char mark;
+
+	protected ChessPiece(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		this.owner = owner;
+		this.game = game;
+		//add game as Observer
+		addObserver(game);
+		this.position = null;
+
+		game.getBoard().placePieceAt(this, init_position);
+	}
+
+	public ChessPlayer getOwner() {
+		return owner;
+	}
+
+	public ChessGame getGame() {
+		return game;
+	}
+
+
+	public ChessPosition getPosition() {
+		return position;
+	}
+
+	public void setPosition(ChessPosition new_position) {
+		position = new_position;
+	}
+
+	public void moveTo(ChessPosition destination) 
+			throws IllegalMove
+			{
+		// Subclass is required to check for legality of move
+		// with respect to piece type, so here we just
+		// make the move generally.
+		if (destination.equals(getPosition())) {
+			throw new IllegalMove(this, getPosition(), destination);
+		}
+
+		ChessBoard board = game.getBoard();
+
+		ChessPiece at_dest = board.getPieceAt(destination);
+
+		if (at_dest != null) {
+			if (at_dest.getOwner() == getOwner()) {
+				throw new IllegalMove(this, getPosition(), destination);
+			}
+			board.removePieceFrom(destination);
+		}
+		ChessPosition pos = getPosition();
+		
+		board.removePieceFrom(getPosition());
+		board.placePieceAt(this, destination);		
+		
+		//Create a ChessMove object
+		ChessMove m = new ChessMove(this, pos, destination, at_dest);
+		
+		//Call setChanged
+		setChanged();
+		//Call notifyObservers and pass ChessMove object
+		notifyObservers(m);
+	}
+	
+	public char getMark() {
+		return mark;
+	}
+
+	protected boolean checkLineOfSight(ChessPosition start, ChessPosition end) throws IllegalMove
+	{
+		// Checks all positions between start and end (exclusive of either
+		// start or end) to make sure they are empty.
+
+		int dx = 0;
+		if (start.getX() < end.getX()) {
+			dx = 1;
+		} else if (start.getX() > end.getX()) {
+			dx = -1;
+		}
+
+		int dy = 0;
+		if (start.getY() < end.getY()) {
+			dy = 1;
+		} else if (start.getY() > end.getY()) {
+			dy = -1;
+		}
+
+		if ((dx != 0) && (dy != 0)) {
+			// If both x and y are changing, then
+			// they must change by the same absolute
+			// amount (i.e., move on a perfect diagonal)
+			if (Math.abs(start.getX() - end.getX()) != Math.abs(start.getY() - end.getY())) {
+				throw new IllegalMove(this, start, end);
+			}
+		}
+
+		ChessPosition next_pos = new ChessPosition(start.getX()+dx, start.getY()+dy);
+		while (!next_pos.equals(end)) {
+			if (getGame().getBoard().getPieceAt(next_pos) != null) {
+				throw new IllegalMove(this, start, end);
+			}
+			next_pos = new ChessPosition(next_pos.getX()+dx, next_pos.getY()+dy);
+		}
+		// If we are here, all is good.
+		return true;
+	}
+}
+
+class Rook extends ChessPiece {
+	public Rook(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'r';
+		} else {
+			mark = 'R';
+		}
+	}	
+
+	@Override
+	public void moveTo(ChessPosition destination) throws IllegalMove
+	{
+		if ((getPosition().getX() != destination.getX()) &&
+				(getPosition().getY() != destination.getY())) {
+			throw new IllegalMove(this, getPosition(), destination);
+		}
+		checkLineOfSight(getPosition(), destination);
+		super.moveTo(destination);
+	}
+	
+	@Override
+	public String toString(){
+		return "rook";
+	}
+}
+
+class Bishop extends ChessPiece {
+	public Bishop(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'b';
+		} else {
+			mark = 'B';
+		}
+	}
+
+	@Override
+	public void moveTo(ChessPosition destination) throws IllegalMove
+	{
+		if ((getPosition().getX() == destination.getX()) ||
+				(getPosition().getY() == destination.getY())) {
+			throw new IllegalMove(this, getPosition(), destination);
+		}
+		//Changes added by rk
+		checkLineOfSight(getPosition(), destination);
+		//end of changes
+		super.moveTo(destination);
+	}
+	
+	@Override
+	public String toString(){
+		return "bishop";
+	}
+}
+
+class Knight extends ChessPiece {
+	public Knight(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'n';
+		} else {
+			mark = 'N';
+		}
+	}
+
+	@Override
+	public void moveTo(ChessPosition destination) throws IllegalMove
+	{
+		int abs_dx = Math.abs(destination.getX()-getPosition().getX());
+		int abs_dy = Math.abs(destination.getY()-getPosition().getY());
+
+		if ((abs_dx + abs_dy != 3) || (abs_dx == 0) || (abs_dy == 0)) {
+			throw new IllegalMove(this, getPosition(), destination);
+		}
+		super.moveTo(destination);
+	}
+	
+	@Override
+	public String toString(){
+		return "knight";
+	}
+}
+
+class Queen extends ChessPiece {
+	public Queen(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'q';
+		} else {
+			mark = 'Q';
+		}
+	}	
+
+	@Override
+	public void moveTo(ChessPosition destination) throws IllegalMove
+	{
+		checkLineOfSight(getPosition(), destination);
+		super.moveTo(destination);
+	}
+	
+	@Override
+	public String toString(){
+		return "queen";
+	}
+}
+
+class King extends ChessPiece {
+	public King(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'k';
+		} else {
+			mark = 'K';
+		}
+	}
+
+	@Override
+	public void moveTo(ChessPosition destination) throws IllegalMove
+	{
+		int abs_dx = Math.abs(destination.getX()-getPosition().getX());
+		int abs_dy = Math.abs(destination.getY()-getPosition().getY());
+
+		if ((abs_dx > 1) || (abs_dy > 1)) {
+			throw new IllegalMove(this, getPosition(), destination);
+		}
+		super.moveTo(destination);
+	}
+	
+	@Override
+	public String toString(){
+		return "king";
+	}
+}
+
+class Pawn extends ChessPiece {
+	public Pawn(ChessPlayer owner, ChessGame game, ChessPosition init_position) {
+		super(owner, game, init_position);
+		if (owner == game.getPlayer1()) {
+			mark = 'p';
+		} else {
+			mark = 'P';
+		}
+	}
+
+	@Override
+	public void moveTo(ChessPosition destination) throws IllegalMove
+	{
+		int dx = destination.getX()-getPosition().getX();
+		int dy = destination.getY()-getPosition().getY();
+
+		if (Math.abs(dy) > 2) {
+			throw new IllegalMove(this, getPosition(), destination);
+		}
+
+		if (getOwner() == getGame().getPlayer1()) {
+			if (dy < 1) {
+				throw new IllegalMove(this, getPosition(), destination);
+			}
+
+			if ((dy == 2) && (getPosition().getY() != 1)) {
+				throw new IllegalMove(this, getPosition(), destination);
+			} 
+		} else {
+			if (dy > -1) {
+				throw new IllegalMove(this, getPosition(), destination);
+			}
+
+			if ((dy == -2) && (getPosition().getY() != 6)) {
+				throw new IllegalMove(this, getPosition(), destination);
+			}
+		}
+
+		if (dx != 0) {
+			// Movement in x direction implies capture.
+			// Must make sure that dx is 1 and that there
+			// is a piece at the destination.
+			// Don't need to worry about whose piece because
+			// superclass will do that for me.
+			if (Math.abs(dx) != 1) {
+				throw new IllegalMove(this, getPosition(), destination);
+			}
+
+			if (getGame().getBoard().getPieceAt(destination) == null) {
+				throw new IllegalMove(this, getPosition(), destination);				
+			}
+		} else {
+			// Here if only moving "forward".
+			// Can't move forward into a piece even regardless
+			// of who owns it, so check to make sure
+			// destination is clear.
+			if (getGame().getBoard().getPieceAt(destination) != null) {
+				throw new IllegalMove(this, getPosition(), destination);
+			}
+		}
+		
+		// May need to check line of sight if moving 2 so do this regardless
+		// This will also prevent trying to move 2 in y direction while
+		// moving 1 in x direction and capturing since move angle will be
+		// illegal according to checkLineOfSight.
+		checkLineOfSight(getPosition(), destination);
+		super.moveTo(destination);
+	}
+	
+	@Override
+	public String toString(){
+		return "pawn";
+	}
+}
